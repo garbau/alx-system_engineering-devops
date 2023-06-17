97c11e369beef7f71bfea8e2c1d8e3608bf628d0
garbau <garbaumar49@gmail.com>
2023-06-17 07:20:25 -0700

    another trial

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,10 @@
-# Your SSH client configuration must be configured
-# to use the private key ~/.ssh/school
-
-include stdlib
-file_line { 'New private key holder':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^IdentityFile',
-  append_on_no_match => true,
-}
-
-file_line { 'No password':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^PasswordAuthentication',
-  append_on_no_match => true,
+# Puppet manifest to configure ssh client with public key authentication
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
